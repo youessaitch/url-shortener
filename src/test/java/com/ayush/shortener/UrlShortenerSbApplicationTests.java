4bb34672a8b09f993ba0e57b9ffa4c8744465557
youessaitch <ayushkumar0077680@gmail.com>
2025-11-18 19:54:38 +0530

    db models setup mysql commit

---
+++ src/test/java/com/ayush/shortener/UrlShortenerSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayush.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
